842829752f1b6b6a168065ecc0e0abafbfe6296b
Mathieu Dique <mathieu.dique@epita.fr>
2024-01-01 23:35:31 +0100

    fix: Fix the down command

--- Controller/Controller.ino
+++ Controller/Controller.ino
@@ -9,14 +9,13 @@
 
 // HTTP Server
 #include "src/server/server.hpp"
-#include "src/api/api.hpp"
 
 // API
-//#include "src/api/api.hpp"
+#include "src/api/api.hpp"
 
 // Wifi settings
-const char *SSID = "***REMOVED***";
-const char *PASSWORD = "***REMOVED***";
+const char* SSID = "***REMOVED***";
+const char* PASSWORD = "***REMOVED***";
 
 // Protobuf buffers size
 const int BUFFER_SIZE = 512;
@@ -24,9 +23,10 @@ const int BUFFER_SIZE = 512;
 // HTTP server global variable
 HTTPServer server = HTTPServer(80);
 
-int FREQ = 50;
-int sens = 1;
-int start = 1;
+const int PWM_FREQ = 50;
+const int SERVO_M_ID = 305;
+const int sens = 1;
+bool TEST_START = true;
 
 // PWM ServoDriver
 Adafruit_PWMServoDriver pwmServo = Adafruit_PWMServoDriver();
@@ -36,56 +36,69 @@ Adafruit_PWMServoDriver pwmServo = Adafruit_PWMServoDriver();
  * ssid Name of the network
  * password Password of the network
  */
-void wifiConnect(const char *ssid, const char *password) {
-  Serial.print("Connecting to ");
-  Serial.println(SSID);
+void wifiConnect(const char* ssid, const char* password)
+{
+    Serial.print("Connecting to ");
+    Serial.println(SSID);
 
-  // Begins a connection
-  WiFi.begin(SSID, PASSWORD);
+    // Begins a connection
+    WiFi.begin(SSID, PASSWORD);
 
-  // Wait until we are connected
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-
-  Serial.println("");
-  Serial.println("WiFi connected.");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-}
-
-void controllerSetup() {
-  pwmServo.begin();
-  pwmServo.setOscillatorFrequency(27000000);
-  pwmServo.setPWMFreq(FREQ);
-}
-
-void setup() {
-  // Serial setup
-  Serial.begin(115200);
-
-  // Wifi setup
-  wifiConnect(SSID, PASSWORD);
-
-  // Server setup
-  serverSetup(&server, &pwmServo);
-
-  // Controller setup
-  controllerSetup();
-
-  Serial.println("Setup done");
-}
-
-void loop() {
-  // Echo server listening
-  if (start == 1) {
-    int motors[] = {0, 3, 7, 11, 15};
-    int SERVOMID = 305;
-    for (int i = 0; i < 3; i++) {
-      pwmServo.setPWM(motors[i], 0, SERVOMID);
+    // Wait until we are connected
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(".");
     }
-    start = 0;
-  }
-  server.loop();
+
+    Serial.println("");
+    Serial.println("WiFi connected.");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+}
+
+void controllerSetup()
+{
+    pwmServo.begin();
+    pwmServo.setOscillatorFrequency(27000000);
+    pwmServo.setPWMFreq(PWM_FREQ);
+}
+
+void setup()
+{
+    // Serial setup
+    Serial.begin(115200);
+
+    // Wifi setup
+    wifiConnect(SSID, PASSWORD);
+
+    // Server setup
+    serverSetup(&server, &pwmServo);
+
+    // Controller setup
+    controllerSetup();
+
+    Serial.println("Setup done");
+}
+
+void loop()
+{
+    if (TEST_START)
+    {
+        int motors[] = { 0, 3, 7, 11, 15 };
+
+        for (int i = 0; i < 3; i++)
+        {
+            pwmServo.setPWM(motors[i], 0, SERVO_M_ID);
+        }
+
+        TEST_START = false;
+    }
+    down(pwmServo, 30);
+    delay(1000);
+    up(pwmServo, 30);
+    delay(1000);
+
+    // Server listening
+    // server.loop();
 }
