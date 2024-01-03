ebd4aba4e1e48ff77cf2d159752623a06f4be043
Mathieu Dique <mathieu.dique@epita.fr>
2024-01-03 20:10:29 +0100

    feat: Thread enhancement

--- controller/src/api/command.cpp
+++ controller/src/api/command.cpp
@@ -1,51 +1,9 @@
 #include "command.hpp"
 
+#include <thread>
+
 #include "../config/config.hpp"
-#include "threads.hpp"
 
-/**
- * Execute a stated thread that will be stoped when notified
- * @param func: the function pointer to the command to execute
- * @param motors: the motors object
- * @param nb_degree: the number of degrees to rotate
- */
-/*
-int statedThread(func fun, Motors motors, int nb_degree)
-{
-    while (!stated_thread.stop)
-    {
-        if (fun(motors, nb_degree) == 1)
-            return 1;
-
-        delay(SERVO_SPEED);
-    }
-
-    return 0;
-}
-*/
-/**
- * Execute a timed thread that will execute during a specific duration
- * @param func: the function pointer to the command to execute
- * @param motors: the motors object
- * @param nb_degree: the number of degrees to rotate
- */
-int timedThread(func fun, Motors motors, int duration)
-{
-    int start = millis();
-    int current = millis();
-
-    while (current - start < duration)
-    {
-        if (fun(motors, 1) == 1)
-            return 1;
-
-        delay(SERVO_SPEED);
-
-        current = millis();
-    }
-
-    return 0;
-}
 /**
  * Parse a Stated command and execute it
  * execute the command if the given `start` parameter of the command is true
@@ -56,7 +14,9 @@ int timedThread(func fun, Motors motors, int duration)
  */
 int command(armwar_StatedCommand command, Motors motors)
 {
-    static Thread* thread = nullptr;
+    // static Thread* thread = nullptr;
+    static bool stop = false;
+    static std::thread thread;
 
     func fun = parse_command(command.command);
 
@@ -65,18 +25,20 @@ int command(armwar_StatedCommand command, Motors motors)
 
     if (command.start)
     {
-        thread = new Thread([=, command, motors, fun]() mutable {
-            fun(motors, 1);
-            delay(SERVO_SPEED);
+        stop = false;
+        thread = std::thread([command, motors, fun, &stop]() mutable {
+            while (!stop)
+            {
+                fun(motors, 1);
+                delay(SERVO_SPEED);
+            }
         });
     }
 
     else
     {
-        thread->stop();
-        thread->join();
-
-        free(thread);
+        stop = true;
+        thread.join();
     }
 
     return 0;
@@ -108,15 +70,13 @@ int command(armwar_SpannedCommand command, Motors motors)
  */
 int command(armwar_TimedCommand command, Motors motors)
 {
-    Thread* thread = nullptr;
-
     func fun = parse_command(command.command);
     int nb_degree = 1;
 
     if (fun == NULL)
         return -1;
 
-    thread = new Thread([=, command, motors, fun]() mutable {
+    std::thread([command, motors, fun]() mutable {
         int start = millis();
         int current = millis();
 
@@ -128,12 +88,7 @@ int command(armwar_TimedCommand command, Motors motors)
 
             current = millis();
         }
-    });
-
-    thread->stop();
-    thread->join();
-
-    free(thread);
+    }).detach();
 
     return 0;
 }
