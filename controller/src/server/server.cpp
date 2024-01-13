11090ccad804673ef902ae8826e99baeb68f2c67
Baptiste Nehmé <baptiste.nehme@epita.fr>
2024-01-13 18:52:26 +0100

    feat(server): add connection logic

--- controller/src/server/server.cpp
+++ controller/src/server/server.cpp
@@ -16,6 +16,10 @@ using namespace httpsserver;
 
 Motors* g_motors;
 
+IPAddress *ip = NULL;
+time_t lastConnectionTime = 0;
+time_t connectionTimeout = DEFAULT_CONNECTION_TIMEOUT;
+
 /**
  * @brief Decode a command from a buffer
  *
@@ -83,6 +87,48 @@ void send_response(HTTPResponse* res, bool success, std::string* message)
     res->write(respBuffer, ostream.bytes_written);
 }
 
+void middlewareAuth(HTTPRequest* req, HTTPResponse* res, std::function<void()> next)
+{
+    // Currently, the middleware is only used to check if the client is
+    // connected to the arm by checking the stored ip address.
+    // There is no security or password check for now.
+    armwar_CommandResponse cmdResponse = armwar_CommandResponse_init_zero;
+    uint8_t respBuffer[SERVER_BUFFER_SIZE] = { 0 };
+    time_t now = time(NULL);
+
+    // If the client is trying to connect and no one is connected or the timeout
+    // of the precedent client was reached, continue
+    if (req->getRequestString().substr(0, 7) == "/login" &&
+        (!ip || difftime(now, lastConnectionTime) > connectionTimeout))
+    {
+        lastConnectionTime = now;
+        next();
+        return;
+    }
+
+    // If no client is connected, no other request are allowed
+    if (!ip)
+    {
+        std::string errorMessage = "You are not connected to the arm.";
+
+        send_response(res, false, &errorMessage);
+        return;
+    }
+
+    // If the client is not the one connected, return an error
+    if (req->getClientIP() != *ip)
+    {
+        std::string errorMessage = "You are not the connected client.";
+
+        send_response(res, false, &errorMessage);
+        return;
+    }
+
+    // If the client is connected, continue
+    lastConnectionTime = now;
+    next();
+}
+
 /**
  * @brief Construct a new Arm War Server object
  *
@@ -100,10 +146,18 @@ void serverSetup(HTTPServer* server, Motors* motors)
         new ResourceNode("/status", "GET", &handleStatus);
     ResourceNode* nodePostRoot =
         new ResourceNode("/command", "POST", &handleCommand);
+    ResourceNode* nodeLoginRoot =
+        new ResourceNode("/login", "POST", &handleLogin);
+    ResourceNode* nodeLogoutRoot =
+        new ResourceNode("/logout", "POST", &handleLogout);
 
     // Register the services
+    server->addMiddleware(&middlewareAuth);
     server->registerNode(nodeGetRoot);
     server->registerNode(nodePostRoot);
+    server->registerNode(nodeLoginRoot);
+    server->registerNode(nodeLogoutRoot);
+
     server->start();
 }
 
@@ -123,6 +177,71 @@ void handleStatus(HTTPRequest* req, HTTPResponse* res)
 
     // res->write(buffer, size);
     res->error();
+}
+
+/**
+ * @brief Handle the login request, storing the client ip
+ *
+ * @param req
+ * @param res
+ */
+void handleLogin(HTTPRequest* req, HTTPResponse* res)
+{
+    armwar_Connect connectMsg = armwar_Connect_init_zero;
+    uint8_t buffer[armwar_Connect_size] = { 0 };
+    pb_istream_t stream;
+    std::string message = "You are now connected to the arm.";
+
+    size_t size = req->readBytes(buffer, armwar_Connect_size);
+    stream = pb_istream_from_buffer(buffer, size);
+
+    if (!pb_decode(&stream, armwar_Connect_fields, &connectMsg))
+    {
+        Serial.println("Failed to decode: ");
+        Serial.println(stream.errmsg);
+        message = "Internal Server Error.";
+        send_response(res, false, &message);
+        return;
+    }
+
+    // Remove the previous client ip if there is one
+    if (ip)
+        delete ip;
+    // Store the client ip
+    ip = new IPAddress(req->getClientIP());
+
+    if (connectMsg.has_timeout)
+        connectionTimeout = connectMsg.timeout;
+
+    Serial.println("Connected client: ");
+    Serial.println(*ip);
+    Serial.println("With connection timeout: ");
+    Serial.println(connectionTimeout);
+
+    send_response(res, true, &message);
+}
+
+/**
+ * @brief Handle the logout request, removing the client ip
+ *
+ * @param req
+ * @param res
+ */
+void handleLogout(HTTPRequest* req, HTTPResponse* res)
+{
+    res->setHeader("Access-Control-Allow-Origin", "*");
+
+    Serial.println("Disconnected client: ");
+    Serial.println(*ip);
+
+    // Remove the client ip
+    if (ip)
+        delete ip;
+    ip = NULL;
+    lastConnectionTime = 0;
+
+    std::string message = "You have been disconnected from the arm.";
+    send_response(res, true, &message);
 }
 
 /**
